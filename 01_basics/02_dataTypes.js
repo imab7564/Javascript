7b6e0350883c0384c3cf4ef8cb08dd8980d80de3
imab7564 <153497091+imab7564@users.noreply.github.com>
2025-01-09 11:26:47 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3)//gives error as we are using nodejs, not browser 
+
+//data types:
+let name="abhishek"
+let age=22//--> this is number 
+let isLoggedIn=false
+let state //=>undefined datatype
+
+//number =>2^53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>means value is not defined
+//symbol =>when to show uniqueness
+
+//object
+
+console.log(typeof "abhishek")//=>string
+console.log(typeof age)//=>number
+console.log(typeof null)//=>object
+console.log(typeof undefined)//=>undefined
+
+
+
