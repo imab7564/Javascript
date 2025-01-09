2c23957f27441d3c7b70de6fb273fa7b2a94f61f
imab7564 <153497091+imab7564@users.noreply.github.com>
2025-01-09 17:56:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof(score));//method k roop me v pass kar sakte h
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" =>33
+//"33abc" =>NaN
+//null =>0
+//undefined =>NaN
+//true =>1,false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+//1 =>true,0 =>false
+//""=>false ,"ab" =>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
