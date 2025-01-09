a280075503a008d87b895ddf792cd0ca7eaf69b6
imab7564 <153497091+imab7564@users.noreply.github.com>
2025-01-09 18:40:29 +0000

    operation

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,30 @@
+let value=3
+let negvalue=-(value)
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2/3);
+
+let str1="hello"
+let str2=" ab"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);//=>12
+console.log(1+"2");//=>12
+console.log("1"+2+2);//=>122
+console.log(1+2+"2");//=>32
+
+console.log(+true);//=>1
+console.log(+"");//=>0
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
