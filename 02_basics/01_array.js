ddc359a942d1cf13d9378347814157eabe4f165a
imab7564 <153497091+imab7564@users.noreply.github.com>
2025-01-18 13:59:33 +0000

    array1

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -27,3 +27,23 @@ console.log(myArr[0]);
 // 7.Join = converts to string.
 // 8. Slice(remove element from s_idx to e_idx but don't include e_idx)= returns new array and original value remains unchanged. Use when you need to extract a portion of an array without modifying it.
 // 9. Splice(value to delete from, number of value to delete) = returns new array with deleted one and original value changed.Use when you need to add, remove, or replace elements within an array.
+
+const newArr=myArr.join()
+console.log(newArr);//0,1,2,3,4,5
+console.log(typeof newArr);//string
+
+
+//slice and splice
+console.log("A:",myArr);//A: [ 0, 1, 2, 3, 4, 5 ]
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);//[1,2]
+console.log("B:",myArr);//B: [ 0, 1, 2, 3, 4, 5 ] means slice doesn't changes original array
+
+const myn2=myArr.splice(1,3)
+console.log(myn2);//[ 1, 2, 3 ]
+console.log("C:",myArr);//C: [ 0, 4, 5 ]
+//iska mtlb h ki splice se original array change ho jata h jo interval dete h wo original array se hat jata h
+
+
+
